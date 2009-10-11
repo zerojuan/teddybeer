236ed592d43f0e5567229215a9e72601d35622e2
juliuscebreros <juliuscebreros@6c190d08-b67e-11de-997e-c5eb2a8ae348>
2009-10-11 16:04:10 +0000

    Initial Commit

---
+++ trunk/TeddyBeer/src/com/icecream/game/TeddyBeerGame.java
@@ -0,0 +1,20 @@
+package com.icecream.game;
+
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.StateBasedGame;
+
+public class TeddyBeerGame extends StateBasedGame{
+
+	public TeddyBeerGame(String name) {
+		super(name);
+		
+	}
+
+	@Override
+	public void initStatesList(GameContainer container) throws SlickException {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
