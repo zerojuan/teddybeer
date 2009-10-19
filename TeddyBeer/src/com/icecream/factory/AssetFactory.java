ee0750edc8d817535ebf683fe8a1cab7b7cd07be
juliuscebreros <juliuscebreros@6c190d08-b67e-11de-997e-c5eb2a8ae348>
2009-10-19 19:31:50 +0000

    Created Spritesheets in AssetFactory.

--- TeddyBeer/src/com/icecream/factory/AssetFactory.java
+++ TeddyBeer/src/com/icecream/factory/AssetFactory.java
@@ -3,9 +3,11 @@ package com.icecream.factory;
 import org.newdawn.slick.Image;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.Sound;
+import org.newdawn.slick.SpriteSheet;
 import org.newdawn.slick.particles.ParticleSystem;
 import org.newdawn.slick.util.Log;
 
+import com.icecream.util.EAnimType;
 import com.icecream.util.EImgType;
 import com.icecream.util.EParticleSysType;
 import com.icecream.util.ESndType;
@@ -21,13 +23,17 @@ public class AssetFactory {
 	private Image playerImage;
 	private Image blockImage;
 
+	private SpriteSheet playerRunning;
+	
 	private static AssetFactory instance;	
 	
 	private AssetFactory() throws SlickException{
 		//Instantiate Images
 		backgroundImage = new Image("assets/background.png");
 		playerImage = new Image("assets/playeranim.png");	
-		
+		playerRunning = new SpriteSheet(
+					playerImage, 
+					288/8, 65,0,0);
 		//Instantiate sound
 		
 		//Instantiate particle systems
@@ -49,6 +55,13 @@ public class AssetFactory {
 			case BACKGROUND: return backgroundImage;
 			case PLAYER: return playerImage;
 			case BLOCK: return blockImage;
+		}
+		return null;
+	}
+	
+	public SpriteSheet getSpriteSheet(EAnimType animType){
+		switch(animType){
+			case PLAYER_RUNNING: return playerRunning; 
 		}
 		return null;
 	}
